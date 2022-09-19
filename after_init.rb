4bcc7ca7365adaacc2c5010186d39e98404e9e1b
lukasch <lukasch987@vivaldi.net>
2022-09-19 14:03:04 +0200

    replaced after_init, added logs

--- after_init.rb
+++ after_init.rb
@@ -1,18 +1,27 @@
 Dir[File.dirname(__FILE__) + '/lib/easyredmine_budget_quotas/easy_patch/*/*/*.rb'].each {|file| require_dependency file }
 
-require_dependency File.dirname(__FILE__) + '/lib/easyredmine_budget_quotas/easy_patch/easy_lookup_budget_quota_activity'
-require_dependency File.dirname(__FILE__) + '/lib/easyredmine_budget_quotas/easy_patch/easy_activity_query'
-require_dependency File.dirname(__FILE__) + '/lib/easyredmine_budget_quotas/easy_patch/modal_selector_controller_extension'
-
-RedmineExtensions::Reloader.to_prepare do
+ActionDispatch::Reloader.to_param do
   require_dependency 'easyredmine_budget_quotas/hooks'
 end
+Rails.logger.warn("#01")
 
-EasyExtensions::EasyLookups::EasyLookup.map do |easy_lookup|
-  easy_lookup.register 'EasyPatch::EasyLookupBudgetQuotaActivity'
+Dir.entries(File.dirname(__FILE__) + '/lib/easyredmine_budget_quotas/easy_patch').each do |file|
+    next unless file.end_with?('.rb')
+    Rails.logger.warn("#02")
+    require_relative "./lib/easyredmine_budget_quotas/easy_patch/#{file}"
 end
+Rails.logger.warn("#03")
 
-ModalSelectorsController.send :include, EasyPatch::ModelSelectorControllerExtension
+#require_dependency 'easyredmine_budget_quotas/easy_patch/easy_lookup_budget_quota_activity'
+EasyExtensions::EasyLookups::EasyLookup.map do |easy_lookup|
+  Rails.logger.warn("BEFORE")
+  easy_lookup.register 'EasyredmineBudgetQuotas::EasyPatch::EasyLookupBudgetQuotaActivity'
+  Rails.logger.warn("AFTER")
+end
+Rails.logger.warn("#04")
+
+#ModalSelectorsController.send :include, EasyredmineBudgetQuotas::EasyPatch::ModelSelectorControllerExtension
+Rails.logger.warn("#05")
 
 
 EasyTimeEntryQuery.class_eval do
@@ -20,5 +29,6 @@ EasyTimeEntryQuery.class_eval do
   def searchable_columns
     ["comments", "projects.name"]
   end
-  
+  Rails.logger.warn("#06")
+
 end
